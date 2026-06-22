13cf5412b9e0a0ba8e83cd4e09c2d50a3f8e27c1
Tijmen Prins <prinstijmen@gmail.com>
2026-06-22 18:20:05 +0200

    Moved scripts

--- Assets/Scripts/LocalPlayer.cs
+++ Assets/Scripts/LocalPlayer.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 
 public class LocalPlayer : MonoBehaviour
 {
-	Client client;
+    Client client;
     CardSprites cardSprites;
 
 
     void Start()
     {
-		client = FindFirstObjectByType<Client>();
+        client = FindFirstObjectByType<Client>();
         cardSprites = FindFirstObjectByType<CardSprites>();
     }
 
     public IEnumerator DrawCards(string[] cards)
     {
+        foreach (string card in cards)
+        {
+            return null;
+
+        }
         return null;
     }
 }
